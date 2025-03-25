5c5fa66f6af7244375ea1ba577f8e30965cd3108
Ayunihshm <b122310472@student.utem.edu.my>
2025-03-25 22:42:42 +0800

    Add NodeJS script and MongoDB connection

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function main() {
     const client = new MongoClient(uri);
 
     try {
+        await client.connect()
         console.log("Connected to MongoDB!");
 
         const db = client.db("testDB");
