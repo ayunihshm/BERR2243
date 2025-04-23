2ce069afcb988ee5ec48522a84cf20e0e2321a96
b122310473@student.utem.edu.my <b122310473@student.utem.edu.my>
2025-04-23 16:10:25 +0800

    Add index4.js

--- index4.js
+++ index4.js
@@ -195,7 +195,7 @@ app.post('/recipes/:recipeId/comments', async (req, res) => {
     await db
       .collection('comments')
       .insertOne({ recipeId: req.params.recipeId, ...req.body });
-    res.status(201).json({ message: 'Comment added' });
+    res.status(201).json({ message: 'Comment added', id: result.insertedId });
   } catch (err) {
     res.status(400).json({ error: 'Invalid comment data' });
   }
